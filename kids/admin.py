95deb0e357a317cf78ec0fa606032ec9a0c504d8
Andrei <freemail_2019@mail.ru>
2021-11-21 22:52:37 +0800

    new

--- kids/admin.py
+++ kids/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Manufacturer)
+admin.site.register(Season)
+admin.site.register()
+admin.site.register()
+admin.site.register()
+admin.site.register()
+admin.site.register()
+admin.site.register()
--- kids/models.py
+++ kids/models.py
@@ -1,12 +1,16 @@
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
+from utils import upload_function
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, verbose_name='Производитель')
     slug = models.SlugField()
     country = models.CharField(max_length=100, verbose_name='Страна производитель')
+    image = models.ImageField(upload_to=upload_function, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Производитель'
@@ -17,7 +21,21 @@ class Manufacturer(models.Model):
 
 
 class Season(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Сезон одежды')
+
+    SEASON_SUMMER = 'summer'
+    SEASON_WINTER = 'winter'
+    SEASON_SPRING = 'spring' """ весна """
+    SEASON_AUTUMN = 'autumn' """ осень """
+
+    STATUS_CHOICE = (
+        (SEASON_SUMMER, 'Лето'),
+        (SEASON_WINTER, 'Зима'),
+        (SEASON_SPRING, 'Весна'),
+        (SEASON_AUTUMN, 'Осень'),
+    )
+
+    name = models.CharField(max_length=100, choices=STATUS_CHOICE, default=SEASON_SUMMER, verbose_name='Сезон одежды')
+    image = models.ImageField(upload_to=upload_function)
 
     class Meta:
         verbose_name = 'Сезон'
@@ -37,6 +55,7 @@ class Products(models.Model):
     stock = models.IntegerField(default=1, verbose_name='Наличие на складе')
     offer_of_the_week = models.BooleanField(default=False, verbose_name='Предложение недели ')
     release_date = models.DateField(verbose_name='Дата выпуса')
+    image = models.ImageField(upload_to=upload_function)
 
     def __str__(self):
         return f"{self.id} | {self.name} | {self.manufacturer.name} | {self.season.name}"
@@ -74,7 +93,7 @@ class CartProduct(models.Model):
 class Cart(models.Model):
     owner = models.ForeignKey('Customer', on_delete=models.CASCADE, verbose_name='Покупатель')
     products = models.ManyToManyField(
-        CartProduct, blank=True, null=True, related_name='related_cart', verbose_name='Продукты для корзины'
+        CartProduct, blank=True, related_name='related_cart', verbose_name='Продукты для корзины'
     )
     total_products = models.IntegerField(default=0, verbose_name='Общее количество товара')
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
@@ -119,7 +138,61 @@ class Order(models.Model):
     address = models.CharField(max_length=255, verbose_name='Адрес')
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, verbose_name='Корзина')
     status = models.CharField(max_length=100, choices=STATUS_CHOICE, default=STATUS_NEW, verbose_name='Статус заказа')
-    buying_type = models.CharField(max_length=100, choices=BUYING_TYPE_CHOICE, default=BUYING_TYPE_SELF, verbose_name='Тип заказа')
+    buying_type = models.CharField(max_length=100, choices=BUYING_TYPE_CHOICE, default=BUYING_TYPE_SELF,
+                                   verbose_name='Тип заказа')
+    comment = models.TextField(null=True, blank=True, verbose_name='Комментарий')
+    created_at = models.DateField(auto_now=True, verbose_name='Дата заказа')
+    order_date = models.DateField(default=timezone.now, verbose_name='Дата получения заказа')
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
 
 
+class Customer(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Покупатель')
+    is_active = models.BooleanField(default=False, verbose_name='В сети ')
+    customer_order = models.ManyToManyField(Order, blank=True, related_name='related_customer',
+                                            verbose_name='Заказы покупателя')
+    wishlist = models.ManyToManyField(Products, blank=True, verbose_name='Список ожидаемого')
+    phone = models.CharField(max_length=20, verbose_name='Номер телефона')
+    address = models.CharField(max_length=255, blank=True, verbose_name='Адрес')
 
+    def __str__(self):
+        return self.user.name
+
+    class Meta:
+        verbose_name = 'Покупатель'
+        verbose_name_plural = 'Покупатели'
+
+
+class Notifications(models.Model):
+    recipient = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name='Получатель')
+    text = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Уведомление для {self.recipient.user.username} | id={self.id}"
+
+
+class Meta:
+    verbose_name = 'Уведомление'
+    verbose_name_plural = 'Уведомления'
+
+
+class ImageGallery(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    image = models.ImageField(upload_to=upload_function)
+    use_in_slider = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Изображение для {self.content_object}"
+
+    class Meta:
+        verbose_name = 'Галерея изображений'
+        verbose_name_plural = verbose_name
